b57b6d88ba9321cce14d5f5e60cbdbf41234606f
Ekaterina <e-kondra@mail.ru>
2021-09-15 22:25:30 +0300

    fix urls.html

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -11,14 +11,14 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')))
 """
-from django.contrib import admin
-from django.urls import path, include
-from mainapp.views import index
+
+from django.urls import path
+from .views import products
+
+app_name = 'products'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='products')),
+    path('', products, name='index'),
 ]
