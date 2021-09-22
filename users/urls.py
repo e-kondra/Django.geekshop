d2fb61f089edb938d7383680bedc44520e220ac9
Ekaterina <e-kondra@mail.ru>
2021-09-22 21:50:17 +0300

    usersapp urls, views

--- users/urls.py
+++ users/urls.py
@@ -11,22 +11,15 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
+from django.urls import path
+from .views import login, register
 
-from mainapp.views import index
+app_name = 'users'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='products')),
-    path('users/', include('users.urls', namespace='users'))
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
