23187e094174d131dbf8b167c3b6d67080f3a018
Ekaterina <e-kondra@mail.ru>
2021-09-25 21:56:31 +0300

    added profiles template

--- users/views.py
+++ users/views.py
@@ -47,6 +47,16 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
+def profile(request):
+    # if request.method == 'POST':
+        # form = UserRegisterForm(data=request.POST)
+
+    context = {
+        'title': 'Geekshop - Профайл'
+    }
+    return render(request, 'users/profile.html', context)
+
+
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
