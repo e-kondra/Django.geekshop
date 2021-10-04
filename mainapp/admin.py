970009b90b4b2166a59c27e552cb6ae4bfc9d790
Ekaterina <e-kondra@mail.ru>
2021-10-04 10:33:52 +0300

    jango's admin some tunes

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -4,4 +4,11 @@ from mainapp.models import ProductCategory,Product
 
 # Register your models here.
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+# admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category') # список колонок для вывода в табличке
+    fields = ('name', 'image', ('price', 'quantity'),'description', 'category' ) # поля в описании продукта
+    readonly_fields = ('description',)
+    ordering = ('name', 'price')
+    search_fields = ('name',)
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from baskets.admin import BasketAdmin
+from baskets.models import Basket
 from .models import User
 
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = Basket
+    inlines = (BasketAdmin,) # вывод по линиям админки корзины
