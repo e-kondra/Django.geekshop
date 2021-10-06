52c79034bc2415a6c30ca36a615253577285b102
Ekaterina <e-kondra@mail.ru>
2021-10-06 15:38:29 +0300

    pagination

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -22,4 +22,5 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
     path('category/<int:category_id>', products, name='category'),
+    path('page/<int:page_id>/', products, name='page'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 import datetime
 import os, json
@@ -17,16 +18,25 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page_id=1):
     #category_id - для фильтра передаем
     # file_path = os.path.join(MODULE_DIR, 'fixtures/db.json')
     products = Product.objects.filter(category_id=category_id) if category_id != None else Product.objects.all()
 
+    paginator = Paginator(products, per_page=3)
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
     context = {
-        'title': 'products',
+        'title': 'Каталог',
         'date': datetime.date.today(),
-        'products': products,
-        'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
+        'categories': ProductCategory.objects.all() }
+    # context.update({'products': products_paginator}) - это другой вариант передачи значения в context
         # загрузка из json
         # 'products': json.load(open(file_path, encoding="utf-8")),
         # заполнение вручную
@@ -34,21 +44,6 @@ def products(request, category_id=None):
         #    {'src': 'vendor/img/products/Adidas-hoodie.png',
         #     'name': 'Худи черного цвета с монограммами adidas Originals',
         #     'price': '6 090,00', 'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-        #    {'src': 'vendor/img/products/Blue-jacket-The-North-Face.png',
-        #     'name': 'Синяя куртка The North Face', 'price': '23 725,00',
-        #     'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-        #    {'src': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-        #     'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-        #     'price': '3 390,00', 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-        #    {'src': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
-        #     'name': 'Черный рюкзак Nike Heritage',
-        #     'price': '2 340,00', 'description': 'Плотная ткань. Легкий материал.'},
-        #    {'src': 'vendor/img/products/Black-Dr-Martens-shoes.png',
-        #     'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-        #     'price': '13 590,00', 'description': 'Гладкий кожаный верх. Натуральный материал.'},
-        #    {'src': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-        #     'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-        #     'price': '2 890,00', 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
         # ],
-    }
+
     return render(request, 'mainapp/products.html', context)
