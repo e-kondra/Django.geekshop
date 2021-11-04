8997e19e665e7a969f0c1fbe89a1a492ba510fe3
Ekaterina <e-kondra@mail.ru>
2021-11-04 18:40:03 +0200

    some corrections cache

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -58,8 +58,8 @@ class Order(models.Model):
     def get_summary(self):
         items = self.orderitems.select_related()
         return {
-            'get_total_cost': sum(list(map(lambda x:x.get_product_cost(),items))),
-            'get_total_quantity': sum(list(map(lambda x:x.quantity, items)))
+            'get_total_cost': sum(list(map(lambda x: x.get_product_cost(), items))),
+            'get_total_quantity': sum(list(map(lambda x: x.quantity, items)))
         }
 
 class OrderItem(models.Model):
